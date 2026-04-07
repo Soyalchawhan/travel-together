1e8cb6b96da8b46f8fcc42f65682cbe5a0cedcef
Soyal <soyalchawhan@gmail.com>
2026-04-07 23:31:06 +0530

    Fix API baseURL and CORS for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://travel-together-api.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    /\.vercel\.app$/
+    'https://travel-together-lovat.vercel.app'
   ],
   credentials: true
 }));
